551ad91cb4983800bca35f8e719664fc2be5c850
Fiki1028 <Fiki.nr13@gmail.com>
2025-11-01 10:57:34 +0700

    Tugas 15 (Laravel CRUD Query Builder)

---
+++ inventoryapp_fiki/resources/views/categories/detail.blade.php
@@ -0,0 +1,12 @@
+@extends('master')
+
+@section('title', 'Detail Category')
+
+@section('content')
+
+    <h1 class="text-primary">{{$category->name}}</h1>
+    <p>{{$category->description}}</p>
+
+    <a href="/category" class="btn btn-secondary btn-sm">Kembali</a>
+
+@endsection
--- inventoryapp_fiki/routes/web.php
+++ inventoryapp_fiki/routes/web.php
@@ -3,9 +3,27 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FormController;
+use App\Http\Controllers\CategoriesController;
 
 Route::get('/', [DashboardController::class, 'dashboard']);
 
 Route::get('/register', [FormController::class, 'register']);
 
 Route::post('/welcome', [FormController::class, 'welcome']);
+
+//CRUD
+
+// create data
+Route::get('/category/create', [CategoriesController::class, 'create']);
+Route::post('/category', [CategoriesController::class, 'store']);
+
+// read data
+Route::get('/category', [CategoriesController::class, 'index']);
+Route::get('/category/{id}', [CategoriesController::class, 'show']);
+
+// update data
+Route::get('/category/{id}/edit', [CategoriesController::class, 'edit']);
+Route::put('/category/{id}', [CategoriesController::class, 'update']);
+
+// delete data
+Route::delete('/category/{id}', [CategoriesController::class, 'destroy']);
